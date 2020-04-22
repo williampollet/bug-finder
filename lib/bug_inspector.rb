bf411969a89390d833d0c5b6df5224a26549fbe4
William Pollet <william.pollet@kisskissbankbank.com>
2020-04-22 18:36:07 +0200

    adding json parse

--- lib/bug_inspector.rb
+++ lib/bug_inspector.rb
@@ -1,6 +1,7 @@
 require 'bug_inspector/configuration'
 require 'http'
 require 'yaml'
+require 'json'
 require 'bug_inspector/version'
 
 module BugInspector
@@ -13,6 +14,19 @@ module BugInspector
 
     def configure
       yield(configuration)
+    end
+
+    def retrieve_errors(method:, exception:, since: Time.now.to_i - 604800, limit: 200, count_only: false)
+      JSON.parse(HTTP.get(
+        configuration.errors_url,
+        params: {
+          action_id: method.gsub('#', '-hash-'),
+          exception: exception,
+          since: since,
+          limit: limit,
+          count_only: count_only,
+        },
+      ).to_s)
     end
   end
 
